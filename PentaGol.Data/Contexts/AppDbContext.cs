6ea10c339344e333ae4153c9dbd37e928d67de1c
Safarmurod7 <sm.ashurov7@gmail.com>
2023-04-29 15:40:07 +0500

    DbContext added

--- PentaGol.Data/Contexts/AppDbContext.cs
+++ PentaGol.Data/Contexts/AppDbContext.cs
@@ -1,13 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using PentaGol.Domain.Entities;
 
 namespace PentaGol.Data.DatabaseConfiguration
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base()
+        { }
+
+        public virtual DbSet<Game> Games { get; set; }
+        public virtual DbSet<Liga> Ligas { get; set; }
+        public virtual DbSet<News> News { get; set; }
+        public virtual DbSet<Team> Teams { get; set; }
 
     }
 }
--- PentaGol.Domain/Entities/Game.cs
+++ PentaGol.Domain/Entities/Game.cs
@@ -6,6 +6,7 @@ public class Game : Auditable
 {
     public DateTime GameDate { get; set; }
     public int FirstTeamId { get; set; }
+
     public int SecondTeamId { get; set; }
     public int FirstTeamScore { get; set; }
     public int SecondTeamScore { get; set; }
