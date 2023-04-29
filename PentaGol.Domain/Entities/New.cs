661dc1da353eda5225eee4b6ebde8cb8efe3a347
Abdulloh <muhamedovabdulloh26@gmail.com>
2023-04-29 12:40:15 +0500

    Entities called Game and New added

---
+++ PentaGol.Domain/Entities/New.cs
@@ -0,0 +1,11 @@
+﻿using PentaGol.Domain.Commons;
+using System.Security.Cryptography.X509Certificates;
+
+namespace PentaGol.Domain.Entities;
+
+public class New : Auditable
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string ImagePath { get; set; }
+}
