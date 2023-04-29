b47b72768558545c004969b7a61248c9c77050d0
Safarmurod7 <sm.ashurov7@gmail.com>
2023-04-29 21:57:58 +0500

    ImageUploadExtension included

---
+++ PentaGol.Service/Extensions/ImageUploadExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+
+
+namespace PentaGol.Service.Extensions;
+
+public static class ImageUploadExtension
+{
+    public static byte[] ToByteArray(this IFormFile file)
+    {
+        using var memoryStream = new MemoryStream();
+        using var fileStream = file.OpenReadStream();
+        fileStream.Seek(0, SeekOrigin.Begin);
+        fileStream.CopyTo(memoryStream);
+        return memoryStream.ToArray();
+    }
+}
