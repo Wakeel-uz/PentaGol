adb0664ef83602f281503a8ca8f00dccce66014a
Safarmurod7 <sm.ashurov7@gmail.com>
2023-04-29 22:27:02 +0500

    NEW CODE IMPLEMENTATION: ImageService and Changes in CreationDTO

--- PentaGol.Service/DTOs/TeamForCreationDto.cs
+++ PentaGol.Service/DTOs/TeamForCreationDto.cs
@@ -1,4 +1,5 @@
-﻿using PentaGol.Domain.Entities;
+﻿using Microsoft.AspNetCore.Http;
+using PentaGol.Domain.Entities;
 using System.ComponentModel.DataAnnotations;
 
 namespace PentaGol.Service.DTOs;
@@ -6,7 +7,7 @@ namespace PentaGol.Service.DTOs;
 public class TeamForCreationDto
 {
     [Required(ErrorMessage = "Image is required")]
-    public string ImagePath { get; set; }
+    public IFormFile Image { get; set; }
     [Required(ErrorMessage = "Club name is required")]
     public string Name { get; set; }
     [Required(ErrorMessage = "Liga Id is required")]
