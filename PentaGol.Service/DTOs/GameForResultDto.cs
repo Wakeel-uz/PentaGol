7ddfcc93750f3177b02b4fb29742f430b88a124c
Abdulloh <muhamedovabdulloh26@gmail.com>
2023-04-29 19:11:50 +0500

    done

--- PentaGol.Service/DTOs/GameForResultDto.cs
+++ PentaGol.Service/DTOs/GameForResultDto.cs
@@ -1,5 +1,16 @@
-﻿namespace PentaGol.Service.DTOs;
+﻿using PentaGol.Domain.Commons;
+
+namespace PentaGol.Service.DTOs;
 
 public class GameForResultDto
 {
+    public int Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime GameDate { get; set; }
+    public int FirstTeamId { get; set; }
+
+    public int SecondTeamId { get; set; }
+    public int FirstTeamScore { get; set; }
+    public int SecondTeamScore { get; set; }
+    public bool IsFinished { get; set; }
 }
--- PentaGol.Service/Mappers/MappingProfile.cs
+++ PentaGol.Service/Mappers/MappingProfile.cs
@@ -1,8 +1,16 @@
 ﻿using AutoMapper;
+using PentaGol.Domain.Entities;
+using PentaGol.Service.DTOs;
 
 namespace PentaGol.Service.Mappers;
 
 public class MappingProfile : Profile
 {
+    public MappingProfile()
+    {
+        CreateMap<Game, GameForCreationDto>().ReverseMap();
+        CreateMap<Game, GameForResultDto>().ReverseMap();
 
+        CreateMap<News, NewsForCreationDto>().ReverseMap();
+    }
 }
