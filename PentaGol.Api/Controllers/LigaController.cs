4b1f189a14ae9dcb569d8615c7adad556f270d9b
Safarmurod7 <sm.ashurov7@gmail.com>
2023-04-30 13:28:14 +0500

    Liga Controller

--- PentaGol.Api/Controllers/LigaController.cs
+++ PentaGol.Api/Controllers/LigaController.cs
@@ -24,19 +24,61 @@ namespace PentaGol.Api.Controllers
             => Ok(new
             {
                 Code = 200,
-                
+                Message = "Success",
+                Data = await this.ligaService.RetrieveByIdAsync(id)
             });
 
         [HttpPost]
-        public async Task<IActionResult> CreateAsync([FromForm]LigaForCreationDto liga)
+        public async Task<IActionResult> CreateAsync([FromForm] LigaForCreationDto liga)
         {
             return Ok(await this.ligaService.CreateAsync(liga));
         }
-        [HttpPost("upload-image")]
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+            => Ok(new
+            {
+                Code = 200,
+                Message = "Success",
+                Data = await this.ligaService.RetrieveAllAsync()
+            });
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int Id)
+            => Ok(new
+            {
+                Code = 200,
+                Message = "Success",
+                Data = await this.ligaService.RemoveAsync(Id)
+            });
+
+        [HttpPost("image-upload")]
         public async Task<IActionResult> ImageUpload([FromForm]LigaImageForCreationDto ligaImage)
         {
             return Ok(await this.ligaService.UploadImageAsync(ligaImage));
         }
+
+        [HttpDelete("image-delete/{ligaId:int}")]
+        public async Task<IActionResult> DeleteLigaImageAsync(int ligaId)
+            => Ok(new
+            {
+                Code = 200,
+                Message = "Success",
+                Data = await this.ligaService.RemoveImageAsync(ligaId)
+            });
+
+        [HttpGet("image-get/{LigaId:int}")]
+        public async Task<IActionResult> GetImageAsync(int ligaImageId)
+            => Ok(new
+            {
+                Code = 200,
+                Message = "Success",
+                Data = await this.ligaService.RetrieveImageAsync(ligaImageId)
+            });
+
+        
+
         [HttpGet("teams-by-score/{LigaId}")]
         public async Task<IActionResult> RetrieveTeamsByScore(int LigaId)
         {
